a86ac60b0d992d8fd6c110154ffe1e55adfd86a7
amelevila <amelevila78@gmail.com>
2025-10-26 00:17:02 +0200

    Fins al pes

--- app/src/main/java/com/adria/activitat04part2/MainActivity.kt
+++ app/src/main/java/com/adria/activitat04part2/MainActivity.kt
@@ -5,23 +5,32 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 
 class MainActivity : AppCompatActivity() {
-    private var isMaleSelected : Boolean = true
-    private var isFemaleSelected : Boolean = false
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+    private var kgWeight: Int = 70
+    private var yearsAge: Int = 18
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+    private lateinit var tvWeight: TextView
+    private lateinit var btnMinusWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private lateinit var tvAge: TextView
+    private lateinit var btnMinusAge: FloatingActionButton
+    private lateinit var btnPlusAge: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initComponents()
-        initListenters()
+        initListeners()
         initUI()
     }
 
@@ -30,9 +39,15 @@ class MainActivity : AppCompatActivity() {
         viewFemale  = findViewById(R.id.viewFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnMinusWeight = findViewById(R.id.btnMinusWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        tvAge = findViewById(R.id.tvAge)
+        btnMinusAge = findViewById(R.id.btnMinusAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
     }
 
-    private fun initListenters() {
+    private fun initListeners() {
         viewMale.setOnClickListener {
             if (isFemaleSelected) {
                 changeGender()
@@ -46,8 +61,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
         rsHeight.addOnChangeListener { _, value, _ ->
-            val cm = value.toInt().toString()
-            tvHeight.text = "$cm cm"
+            val cmHeight = value.toInt().toString()
+            tvHeight.text = "$cmHeight cm"
+        }
+        btnMinusWeight.setOnClickListener {
+            kgWeight--
+            tvWeight.text = "$kgWeight kg"
+        }
+        btnPlusWeight.setOnClickListener {
+            kgWeight++
+            tvWeight.text = "$kgWeight kg"
+        }
+        btnMinusAge.setOnClickListener {
+            yearsAge--
+            tvAge.text = "$yearsAge"
+        }
+        btnPlusAge.setOnClickListener {
+            yearsAge++
+            tvAge.text = "$yearsAge"
         }
     }
 
